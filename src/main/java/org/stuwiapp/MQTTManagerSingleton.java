01a31d0e720155c9032bd073471b877376d599e2
Erik Nisbet <eriknisbet8@gmail.com>
2024-04-11 14:48:51 +0200

    Add missing implementation of MQTTManagerSingleton

--- src/main/java/org/stuwiapp/MQTTManagerSingleton.java
+++ src/main/java/org/stuwiapp/MQTTManagerSingleton.java
@@ -1,2 +1,28 @@
-package org.stuwiapp;public class MQTTHandlerSingleton {
+package org.stuwiapp;
+
+import org.eclipse.paho.client.mqttv3.MqttException;
+
+/***
+ * This class ensures theres only one instance of the MQTTManager
+ * as to not create several connections
+ */
+
+public class MQTTManagerSingleton {
+    private static final MQTTManager MQTT_INSTANCE;
+
+    private MQTTManagerSingleton() {
+    }
+
+    static {
+        try {
+            MQTT_INSTANCE = new MQTTManager();
+        } catch (MqttException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // function to get singleton instance of MQTTManager
+    public static MQTTManager getMqttInstance() {
+        return MQTT_INSTANCE;
+    }
 }
