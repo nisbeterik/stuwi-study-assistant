15b23f58ecefe6b80282f7f9773c3c89c18bfb97
Erik Nisbet <eriknisbet8@gmail.com>
2024-04-11 14:22:06 +0200

    #17 Implement MQTTManager >> Implement subscribe/publish logic in StuWi App. >> Verified connectivity between Wio and StuWi App. See screenshots in related issue.

--- src/main/java/org/stuwiapp/MQTTManager.java
+++ src/main/java/org/stuwiapp/MQTTManager.java
@@ -1,2 +1,65 @@
-package org.stuwiapp;public class MQTTManager {
+package org.stuwiapp;
+
+import org.eclipse.paho.client.mqttv3.*;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+public class MQTTManager {
+    private final String BROKER ="tcp://broker.hivemq.com:1883" ;
+    private String clientId;
+    private final int QOS = 0;  // 0 sends message at most once
+    private MqttClient client;
+
+    public MQTTManager() throws MqttException{
+        this.clientId = "StuWiApp";
+        initiateMQTTClient();
+    }
+
+    public void initiateMQTTClient() throws MqttException{
+        this.client = new MqttClient(BROKER, clientId, new MemoryPersistence());
+
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setAutomaticReconnect(true); // sets client to automatically reconnect
+        options.setCleanSession(true);  // remembers previous state after reconnects
+
+        client.setCallback(callback());
+
+        System.out.println("Connecting to broker: " + BROKER);
+        client.connect(options);    // connects with connect options
+        System.out.println("Connected");
+    }
+
+    public void publish(String topic, String payload) throws MqttException{
+        MqttMessage message = new MqttMessage(payload.getBytes());
+        message.setQos(QOS);
+        client.publish(topic, message);
+    }
+
+    public void subscribe(String topic) throws MqttException{
+        this.client.subscribe(topic);
+    }
+
+    public void close() throws MqttException{
+
+        client.disconnect();
+        client.close();
+    }
+
+    // enables asynchronous events related to mqttclient
+    private MqttCallback callback() {
+        return new MqttCallback() {
+            public void connectionLost(Throwable cause) {
+                System.out.println("Connection lost: " + cause.getMessage());
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                System.out.println("Message arrived. Topic: " + topic + " Message: " + message.toString());
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+                System.out.println("Delivery complete. ");
+            }
+        };
+    }
 }
--- src/main/java/org/stuwiapp/StuWiApp.java
+++ src/main/java/org/stuwiapp/StuWiApp.java
@@ -5,12 +5,27 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import org.eclipse.paho.client.mqttv3.MqttException;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * JavaFX App
  */
 public class StuWiApp extends Application {
 
+    private MQTTManager mqttManager;
+    private String publishTopic = "stuwi/testin"; // topic that WIO subscribes to
+    private String subscribeTopic = "stuwi/testout"; // topic that WIO publishes to
+    private final ScheduledExecutorService publishScheduler = Executors.newSingleThreadScheduledExecutor();
+
+    @Override
+    public void init() throws MqttException {
+        mqttManager = new MQTTManager();
+        mqttManager.subscribe(subscribeTopic);
+    }
     @Override
     public void start(Stage stage) {
         var javaVersion = SystemInfo.javaVersion();
@@ -20,6 +35,15 @@ public class StuWiApp extends Application {
         var scene = new Scene(new StackPane(label), 640, 480);
         stage.setScene(scene);
         stage.show();
+
+        // publishes message every 10 seconds to publish topic
+        publishScheduler.scheduleAtFixedRate(() -> {
+            try {
+                mqttManager.publish(publishTopic, "Message from StuWi app");
+            } catch (MqttException e) {
+                e.printStackTrace();
+            }
+        }, 0, 10, TimeUnit.SECONDS);
     }
 
     public static void main(String[] args) {
