12ee6d939e9e8b6d1228a43d7dc65bfd642dca47
Martin Lidgren <martin.lidgren@live.se>
2024-04-12 13:07:38 +0200

    #6 Add initial dashboard for displaying temperature and humidity. Add controller for dashboard.

---
+++ src/main/java/org/stuwiapp/controller/DashboardController.java
@@ -0,0 +1,9 @@
+package org.stuwiapp.controller;
+
+import javafx.scene.control.Label;
+
+public class DashboardController {
+    public Label tempReadingLabel;
+    public Label versionLabel;
+    public Label humiReadingLabel;
+}
