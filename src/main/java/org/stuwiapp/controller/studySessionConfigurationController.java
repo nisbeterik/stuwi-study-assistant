decc2a8cdf0182fd0d4688ac30b3c6b3d0d3e3d2
eddedomer <edvsan@student.chalmers.se>
2024-04-24 15:43:54 +0200

    #9 Add listener for slider so that indicator labels display current slected values in studySessionCondig view

--- src/main/java/org/stuwiapp/controller/studySessionConfigurationController.java
+++ src/main/java/org/stuwiapp/controller/studySessionConfigurationController.java
@@ -1,8 +1,14 @@
 package org.stuwiapp.controller;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.control.*;
+import javafx.fxml.Initializable;
 
-public class studySessionConfigurationController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class studySessionConfigurationController extends ParentController implements Initializable  {
 
     public Button saveTemplateButton;
     public Label titleLabel;
@@ -21,7 +27,28 @@ public class studySessionConfigurationController {
     public Label breakDurationIndicator;
     public Label breakIntervalIndicator;
 
-    public void saveSettingsAsTemplate(){
+    public void initialize(URL url, ResourceBundle resourceBundle){
+        durationSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
+                sessionDurationIndicator.setText((int)durationSlider.getValue() + " m");
+            }
+        });
+        breakDurationSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
+                breakDurationIndicator.setText((int)breakDurationSlider.getValue() + " m");
+            }
+        });
+        breakIntervalSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
+                breakIntervalIndicator.setText((int)breakIntervalSlider.getValue() + " m");
+            }
+        });
+    }
+
+    public void saveSettingsAsTemplate() {
 
     }
 }
