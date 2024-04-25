16d790c8cdef24557630addf792b184961ca2974
eddedomer <edvsan@student.chalmers.se>
2024-04-25 11:35:31 +0200

    #9 Change studysession template structure to blocks, breaks and duration. update necessary slider, labels etc...

--- src/main/java/org/stuwiapp/StudySessionTemplate.java
+++ src/main/java/org/stuwiapp/StudySessionTemplate.java
@@ -3,33 +3,28 @@ package org.stuwiapp;
 public class StudySessionTemplate {
     private String title;
     private String subject;
-    private int duration;
-    private int breakInterval;
+    private int blockDuration;
+    private int blocks;
     private int breakDuration;
-    final static int MINUTE_MULTIPLIER = 60;
-    final static int MAX_BREAK_INTERVAL = 60;
+    final static int MIN_BREAK_INTERVAL = 5;
     final static int MAX_DUR_NO_BREAK = 60;
 
-    public StudySessionTemplate(String title, String subject, int durationMinutes, int breakIntervalMinutes, int breakDuration) throws Exception{
-        if (breakIntervalMinutes > durationMinutes){
-            throw new Exception("Break interval longer than study sessions total duration");
-        } if (breakIntervalMinutes > MAX_BREAK_INTERVAL) {
-            throw new Exception("Break interval longer than max break interval of 60 minutes");
-        } if (durationMinutes > MAX_DUR_NO_BREAK){
-            if (breakIntervalMinutes == 0){
-                throw new Exception("breaks must be set for study sessions longer than 60 minutes");
+    public StudySessionTemplate(String title, String subject, int blockDuration, int breakDuration, int blocks) throws Exception{
+        if (blocks > 1){
+            if (breakDuration < 5){
+                throw new Exception("At least 5 min breaks must be set for study sessions with more than 1 block.");
             }
         }
         this.title = title;
         this.subject = subject;
-        this.duration = durationMinutes;
-        this.breakInterval = breakIntervalMinutes;
+        this.blockDuration = blockDuration;
         this.breakDuration = breakDuration;
+        this.blocks = blocks;
     }
 
-    public int getBreakInterval() {
+    public int getBlocks() {
         //returns break interval in minutes
-        return breakInterval;
+        return blocks;
     }
     public int getBreakDuration(){
         //returns break interval in minutes
@@ -37,7 +32,7 @@ public class StudySessionTemplate {
     }
     public int getDuration() {
         //returns duration in minutes
-        return duration;
+        return blockDuration;
     }
     public String getSubject(){
         return subject;
--- src/main/java/org/stuwiapp/controller/studySessionConfigurationController.java
+++ src/main/java/org/stuwiapp/controller/studySessionConfigurationController.java
@@ -22,13 +22,13 @@ public class studySessionConfigurationController extends ParentController implem
     @FXML public Slider durationSlider;
     @FXML public Slider breakDurationSlider;
     @FXML public Label infoLabel;
-    @FXML public Label breakIntervalLabel;
-    @FXML public Slider breakIntervalSlider;
+    @FXML public Label blocksLabel;
+    @FXML public Slider blocksSlider;
     @FXML public Label breakDurationLabel;
     @FXML public Button startSessionButton;
     @FXML public Label sessionDurationIndicator;
     @FXML public Label breakDurationIndicator;
-    @FXML public Label breakIntervalIndicator;
+    @FXML public Label blocksIndicator;
 
     public studySessionConfigurationController(){
 
@@ -38,7 +38,7 @@ public class studySessionConfigurationController extends ParentController implem
         //TODO: for each saved template add as item to the templateChoiceBox.
 
         try {
-            templateChoiceBox.getItems().add(new StudySessionTemplate("Recommended Settings", "General", 120, 30, 5 ));
+            templateChoiceBox.getItems().add(new StudySessionTemplate("Recommended Settings", "General", 25, 5, 4 ));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -60,17 +60,17 @@ public class studySessionConfigurationController extends ParentController implem
                 breakDurationIndicator.setText((int)breakDurationSlider.getValue() + " m");
             }
         });
-        breakIntervalSlider.valueProperty().addListener(new ChangeListener<Number>() {//Slider listener tu update indicator
+        blocksSlider.valueProperty().addListener(new ChangeListener<Number>() {//Slider listener tu update indicator
             @Override
             public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
-                breakIntervalIndicator.setText((int)breakIntervalSlider.getValue() + " m");
+                blocksIndicator.setText((int)blocksSlider.getValue() + " m");
             }
         });
     }
     public boolean loadStudyTemplate(StudySessionTemplate template){
         if (template == null) {return false;}
         durationSlider.setValue(template.getDuration());
-        breakIntervalSlider.setValue(template.getBreakInterval());
+        blocksSlider.setValue(template.getBlocks());
         breakDurationSlider.setValue(template.getBreakDuration());
         subjectField.setText(template.getSubject());
 
@@ -80,12 +80,12 @@ public class studySessionConfigurationController extends ParentController implem
     public StudySessionTemplate saveSettingsAsTemplate() {
         String title = "title";
         int duration = (int)durationSlider.getValue();
-        int breakInterval = (int)breakIntervalSlider.getValue();
+        int blocks = (int)blocksSlider.getValue();
         int breakDuration = (int)breakDurationSlider.getValue();
         String subject = subjectField.getText();
 
         try {
-            StudySessionTemplate studySessionTemplate = new StudySessionTemplate(title, subject, duration, breakInterval, breakDuration);
+            StudySessionTemplate studySessionTemplate = new StudySessionTemplate(title, subject, duration, breakDuration, blocks);
             infoLabel.setText("Successfully saved template " + subject);
             return studySessionTemplate;
 
