fd408e30e476326de527486b9ed7606d4b586a74
Erik Nisbet <eriknis@student.chalmers.se>
2024-04-26 16:55:43 +0200

    #11 Remove delay() calls in play_defauly_sound()
    Add logic making use of current_time to calculate duration of buzzer

--- src/wio-StuWi/src/stuwi_sketch_main/buzzer_handler.cpp
+++ src/wio-StuWi/src/stuwi_sketch_main/buzzer_handler.cpp
@@ -1,16 +1,25 @@
 #include "buzzer_handler.h"
 #include "wifi.h"
+#include "rtc_handler.h"
 #define BUZZER_PIN WIO_BUZZER
+#define SECOND 1
+
+DateTime sound_duration;
 
 void buzzer_setup() {
     pinMode(BUZZER_PIN, OUTPUT);
+    
 }
 
 void play_default_sound(){
-        analogWrite(WIO_BUZZER, 128);
-        delay(1000);
+
+        sound_duration = DateTime(current_time + 1);
+        while(current_time.unixtime() != sound_duration.unixtime()) {
+            analogWrite(WIO_BUZZER, 128);
+        }
+        
         analogWrite(WIO_BUZZER, 0);
-        delay(1000);
+        
         
       }
 
