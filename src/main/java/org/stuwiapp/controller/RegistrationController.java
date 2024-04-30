f9c9d091cfcacc0e8451ced6dcf47199a73eb067
Martin Lidgren <marlidg@student.chalmers.se>
2024-04-30 12:54:13 +0200

    #26 Implement page and controller for registration of new users

--- src/main/java/org/stuwiapp/controller/RegistrationController.java
+++ src/main/java/org/stuwiapp/controller/RegistrationController.java
@@ -1,4 +1,39 @@
 package org.stuwiapp.controller;
 
-public class RegistrationController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import org.stuwiapp.database.UserDAO;
+
+public class RegistrationController extends ParentController{
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private Button registerButton;
+    @FXML
+    private Button cancelButton;
+
+    public void registerNewUser(ActionEvent event) {
+        String enteredUsername = usernameField.getText().trim();
+        String enteredPassword = passwordField.getText();
+        boolean registrationSuccess = false;
+
+        try {
+            registrationSuccess = UserDAO.registerUser(enteredUsername, enteredPassword);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (registrationSuccess){
+            redirect(event, "stuwi-home.fxml");
+        }
+    }
+
+    public void returnToLogin(ActionEvent event) {
+        redirect(event, "login.fxml");
+    }
 }
