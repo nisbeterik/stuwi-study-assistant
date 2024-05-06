e4faf916ea0378b47972ccfbc847c30adf3b95e1
Erik Nisbet <eriknis@student.chalmers.se>
2024-05-06 16:21:30 +0200

    #27 Remove redundant extern declarations in screen_draw.h

--- src/wio-StuWi/src/stuwi_sketch_main/screen_draw.h
+++ src/wio-StuWi/src/stuwi_sketch_main/screen_draw.h
@@ -4,10 +4,6 @@
 
 #include "TFT_eSPI.h"
 
-extern TFT_eSPI tft;
-extern char temp_string[20];
-extern char humid_string[20];
-extern char loudness_string[20];
 
 extern void screen_setup();
 extern void draw_background();
