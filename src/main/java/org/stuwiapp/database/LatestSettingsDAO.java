c58b6ff206f66f2f4e87b1c3d5e9a70e66df04ec
eddedomer <edvsan@student.chalmers.se>
2024-05-08 13:26:44 +0200

    #30 add correct imports for MongoDB access

--- src/main/java/org/stuwiapp/database/LatestSettingsDAO.java
+++ src/main/java/org/stuwiapp/database/LatestSettingsDAO.java
@@ -1,4 +1,16 @@
 package org.stuwiapp.database;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.json.JSONObject;
+import org.stuwiapp.StudySessionTemplate;
+import org.stuwiapp.RangeSettingsTemplate;
+
+import java.util.ArrayList;
+
 public class LatestSettingsDAO {
+
+
 }
