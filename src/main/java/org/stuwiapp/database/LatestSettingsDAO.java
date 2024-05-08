41b170e76c8738cbe98e9edf81d9114f9c029709
eddedomer <edvsan@student.chalmers.se>
2024-05-08 13:36:07 +0200

    #30 add function to delete latest document before creating a new one.

--- src/main/java/org/stuwiapp/database/LatestSettingsDAO.java
+++ src/main/java/org/stuwiapp/database/LatestSettingsDAO.java
@@ -3,7 +3,9 @@ package org.stuwiapp.database;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.json.JSONObject;
 import org.stuwiapp.StudySessionTemplate;
 import org.stuwiapp.RangeSettingsTemplate;
@@ -12,14 +14,21 @@ import java.util.ArrayList;
 
 public class LatestSettingsDAO {
 
+    private static MongoClient client = MongoConnectionManager.getMongoClient();
+    private static MongoDatabase db = client.getDatabase("stuwi");
+    private static MongoCollection<Document> collection = db.getCollection("latestTemplates");
+
     public static void saveLatestRangeSettings(RangeSettingsTemplate template, String user){
-        MongoClient client = MongoConnectionManager.getMongoClient();
-        MongoDatabase db = client.getDatabase("stuwi");
-        MongoCollection<Document> collection = db.getCollection("latestTemplates");
+
+
+        if (collection.countDocuments() > 0) {
+            Bson filter = Filters.eq("title", "LATESTRANGES");
+            collection.deleteOne(filter);
+        }
 
         JSONObject templateJson = new JSONObject();
         templateJson.put("user", user);
-        templateJson.put("title", template.getTitle());
+        templateJson.put("title", "LATESTRANGES");
         templateJson.put("tempMax", template.getTempMax());
         templateJson.put("tempMin", template.getTempMin());
         templateJson.put("humidMax", template.getHumidMax());
