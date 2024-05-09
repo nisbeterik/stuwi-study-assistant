34482b6a2462714fabd06469de872a6c73cdf3e0
eddedomer <edvsan@student.chalmers.se>
2024-05-09 11:36:45 +0200

    #36 make the background anchorpane stylable through .pane for registration.fxml

--- src/main/java/org/stuwiapp/controller/RegistrationController.java
+++ src/main/java/org/stuwiapp/controller/RegistrationController.java
@@ -2,13 +2,19 @@ package org.stuwiapp.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import org.stuwiapp.database.UserDAO;
 
-public class RegistrationController extends ParentController{
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RegistrationController extends ParentController implements Initializable {
+    public AnchorPane background;
     @FXML
     private TextField usernameField;
     @FXML
@@ -17,6 +23,11 @@ public class RegistrationController extends ParentController{
     private Button registerButton;
     @FXML
     private Button cancelButton;
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        background.getStyleClass().add("pane");
+    }
+
 
     public void registerNewUser(ActionEvent event) {
         String enteredUsername = usernameField.getText().trim();
