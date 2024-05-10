bc30f59ea848b315dec1194192e9608e715e0c41
Erik Nisbet <eriknis@student.chalmers.se>
2024-05-10 13:10:44 +0200

    #40 Add labels and buttons, initialize labels

--- src/main/java/org/stuwiapp/controller/StudySessionGraphController.java
+++ src/main/java/org/stuwiapp/controller/StudySessionGraphController.java
@@ -2,18 +2,27 @@ package org.stuwiapp.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import org.stuwiapp.service.StudySessionAnalyticsService;
 
 public class StudySessionGraphController {
 
     public LineChart durationChart;
+    public Label hoursAndMinutesLabel;
+    public Label averageRatingNumberLabel;
+    public Button returnHomeButton;
 
     StudySessionAnalyticsService service = new StudySessionAnalyticsService();
     @FXML
     public void initialize() {
+        hoursAndMinutesLabel.setText(service.totalStudiedInHoursAndMinutes());
+        averageRatingNumberLabel.setText(service.averageRating());
 
 
+    }
 
-
+    public void returnHome(MouseEvent mouseEvent) {
     }
 }
