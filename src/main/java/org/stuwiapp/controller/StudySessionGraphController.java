a655326d87c1b12cd806b65ef4b0326b69e51452
Erik Nisbet <eriknis@student.chalmers.se>
2024-05-10 14:48:15 +0200

    #40 Add XAxis functionality

--- src/main/java/org/stuwiapp/controller/StudySessionGraphController.java
+++ src/main/java/org/stuwiapp/controller/StudySessionGraphController.java
@@ -1,5 +1,6 @@
 package org.stuwiapp.controller;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.chart.*;
 import javafx.scene.control.Button;
@@ -7,15 +8,16 @@ import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import org.stuwiapp.service.StudySessionAnalyticsService;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class StudySessionGraphController extends ParentController {
 
 
-    public StackedBarChart<String, Integer> barChart;
-    public CategoryAxis categoryAxis;
-    public NumberAxis numberAxis;
+    public StackedBarChart<String, Number> barChart;
+    public CategoryAxis X;
+    public NumberAxis Y;
     @FXML
     private Label hoursAndMinutesLabel;
 
@@ -28,28 +30,29 @@ public class StudySessionGraphController extends ParentController {
     StudySessionAnalyticsService service = new StudySessionAnalyticsService();
     @FXML
     public void initialize() {
-
+        X = (CategoryAxis) barChart.getXAxis();
         List<Integer> studyTimeData = service.calculateTotalStudyTimePerDay();
         Collections.reverse(studyTimeData);
         System.out.println(studyTimeData);
+        configureCategoryAxis();
         displayStudyTimeData(studyTimeData);
+
 
         hoursAndMinutesLabel.setText(service.totalStudiedInHoursAndMinutes());
         averageRatingNumberLabel.setText(service.averageRating());
 
 
+
+
     }
 
     private void displayStudyTimeData(List<Integer> studyTimeData) {
-        XYChart.Series<String, Integer> series = new XYChart.Series<>();
+        XYChart.Series<String, Number> series = new XYChart.Series<>();
+        series.setName("Duration");
 
         for (int day = 1; day <= studyTimeData.size(); day++) {
             int studiedDuration = studyTimeData.get(day - 1);
-
-
-            if (studiedDuration >= 0) {
-                System.out.println(series.getData().add(new XYChart.Data<>(String.valueOf(day), studiedDuration)));
-            }
+            series.getData().add(new XYChart.Data<>(String.valueOf(day), studiedDuration));
         }
 
 
@@ -59,5 +62,18 @@ public class StudySessionGraphController extends ParentController {
 
     public void returnHome(MouseEvent mouseEvent) {
         redirect(mouseEvent, "stuwi-home.fxml");
+    }
+
+    private void configureCategoryAxis() {
+        List<String> dayLabels = generateDayLabels(1, 31);
+        X.setCategories(FXCollections.observableArrayList(dayLabels));
+    }
+
+    private List<String> generateDayLabels(int startDay, int endDay) {
+        List<String> labels = new ArrayList<>();
+        for (int i = startDay; i <= endDay; i++) {
+            labels.add(String.valueOf(i));
+        }
+        return labels;
     }
 }
