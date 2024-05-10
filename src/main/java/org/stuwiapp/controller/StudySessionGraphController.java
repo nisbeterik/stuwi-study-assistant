9c350d667dc9cc5548a0a404a523daa96309aaf3
Erik Nisbet <eriknis@student.chalmers.se>
2024-05-10 13:05:08 +0200

    #40 Fix name of StudySessionGraphController.jav

--- src/main/java/org/stuwiapp/controller/StudySessionGraphController.java
+++ src/main/java/org/stuwiapp/controller/StudySessionGraphController.java
@@ -1,2 +1,19 @@
-package org.stuwiapp.controller;public class StudySessionAnalyticsController {
+package org.stuwiapp.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import org.stuwiapp.service.StudySessionAnalyticsService;
+
+public class StudySessionGraphController {
+
+    public LineChart durationChart;
+
+    StudySessionAnalyticsService service = new StudySessionAnalyticsService();
+    @FXML
+    public void initialize() {
+
+
+
+
+    }
 }
